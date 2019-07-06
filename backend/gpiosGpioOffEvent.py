25cc7aaa4840ec4ff537a85c69bfc58ce442ea25
Tangb <tanguy.bonneau@gmail.com>
2019-07-06 10:39:41 +0200

    Update events after core changes

--- backend/gpiosGpioOffEvent.py
+++ backend/gpiosGpioOffEvent.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from raspiot.events.event import Event
+from raspiot.libs.internals.event import Event
 
 class GpiosGpioOffEvent(Event):
     """
@@ -10,6 +10,7 @@ class GpiosGpioOffEvent(Event):
 
     EVENT_NAME = u'gpios.gpio.off'
     EVENT_SYSTEM = False
+    EVENT_PARAMS = [u'gpio', u'init', u'duration']
 
     def __init__(self, bus, formatters_broker, events_broker):
         """ 
@@ -21,21 +22,4 @@ class GpiosGpioOffEvent(Event):
             events_broker (EventsBroker): events broker instance
         """
         Event.__init__(self, bus, formatters_broker, events_broker)
-
-    def _check_params(self, params):
-        """
-        Check event parameters
-
-        Args:
-            params (dict): event parameters
-
-        Return:
-            bool: True if params are valid, False otherwise
-        """
-        keys = [
-            u'gpio',
-            u'init',
-            u'duration'
-        ]
-        return all(key in keys for key in params.keys())
 
